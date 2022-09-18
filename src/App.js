b8254abffa2e6dc350db49b26297e20a440cac04
virendra singh shekhawat <107749382+virendrasinghshekhawat@users.noreply.github.com>
2022-09-18 06:39:12 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import NavBar from './Components/NavBar';
+import Search from './Components/Search';
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Search />
+    </>
+  );
+}
+
+export default App;
